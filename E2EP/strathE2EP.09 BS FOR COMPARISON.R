6fc7dcea1281c20f401aee58bd323aebaf06f8d6
Matty Hatton <94123416+Matthew-Hatton@users.noreply.github.com>
2023-08-22 10:21:50 +0100

    error in run function

--- E2EP/strathE2EP.09 BS FOR COMPARISON.R
+++ E2EP/strathE2EP.09 BS FOR COMPARISON.R
@@ -1,5 +1,8 @@
-model_BS <- e2ep_read("Barents_Sea","2011-2019")
-results_BS <- e2ep_run(model,nyears = 10)
 
-saveRDS(results_BS,"Models\\BarentsSea2011Modelresults.rds")
-saveRDS(model_BS,"Models\\BarentsSea2011Model.rds")
+model_BS <- e2ep_read("Barents_Sea","2011-2019",models.path = "Models")
+results_BS <- e2ep_run(model_BS,nyears = 10)
+e2ep_plot_ts(model_BS,results_BS,selection = "ECO")
+
+# 
+# saveRDS(results_BS,"Models\\BarentsSea2011Modelresults.rds")
+# saveRDS(model_BS,"Models\\BarentsSea2011Model.rds")
