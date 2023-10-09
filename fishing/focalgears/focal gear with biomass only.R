2dbb6b2c27232e5e9ed7f88284fe642c891ed8a5
Matty Hatton <94123416+Matthew-Hatton@users.noreply.github.com>
2023-10-09 11:15:14 +0100

    Fixed Pelagic_trawl.seine name bug

--- fishing/focalgears/focal gear with biomass only.R
+++ fishing/focalgears/focal gear with biomass only.R
@@ -22,26 +22,13 @@ gears <- c("Pelagic_trawl+seine",
            "Rifles",
            "Kelp_harvester"
 )
-
-master <- data.frame()
-for (i in 1:length(gears)){
-  randdf <- matrix(nrow = length(scaling),ncol = length(gears),data = runif(n = length(scaling)*length(gears),min=scaling[1],
-                                                                            max = tail(scaling,n=1))) %>%
-    data.frame() #creates dataframe with correct dimensions
-  randdf[,i] <- scaling
-  randdf$focal <- gears[i]
-  master <- rbind(master,randdf)
-} #build the random dataframe
-colnames(master) <- append(gears,"focal")
-#give ID number
-master <- tibble::rowid_to_column(master,"SimulationID") #adds simID
-rm(randdf)
-
-focalgear <- function(SimulationID,`Pelagic_trawl+seine`,`Demersal_otter_trawl`,`Demersal_seine`,
+master <- read.csv("./fishing/fishing bounds/changingpower/mastercombinationsdf.csv") %>% #loads in master df
+  .[,-1] #use same master dataframe as other simulation
+focalgear <- function(SimulationID,`Pelagic_trawl.seine`,`Demersal_otter_trawl`,`Demersal_seine`,
                       `Gill_nets`,`Longlinesandjiggiing`,`Recreational`,`Shrimp_trawl`,
                       `Creels`,`Mollusc_dredge`,`Harpoons`,`Rifles`,`Kelp_harvester`,focal){
   
-  instructions <- c(`Pelagic_trawl+seine`,`Demersal_otter_trawl`,`Demersal_seine`,
+  instructions <- c(`Pelagic_trawl.seine`,`Demersal_otter_trawl`,`Demersal_seine`,
                     `Gill_nets`,`Longlinesandjiggiing`,`Recreational`,`Shrimp_trawl`,
                     `Creels`,`Mollusc_dredge`,`Harpoons`,`Rifles`,`Kelp_harvester`)
   model <- e2ep_read("SW_Greenland","2011-2019",
@@ -52,10 +39,11 @@ focalgear <- function(SimulationID,`Pelagic_trawl+seine`,`Demersal_otter_trawl`,
   results <- e2ep_run(model,nyears = 50) #run model
   
   #saves outputs
-  saveRDS(results$final.year.outputs$mass_results_offshore,str_glue("./fishing/fishing bounds/changingpower/biomassoutputs/offshore/{SimulationID}_offshore_{focal}.rds"))
-  saveRDS(results$final.year.outputs$mass_results_inshore,str_glue("./fishing/fishing bounds/changingpower/biomassoutputs/inshore/{SimulationID}_inshore_{focal}.rds"))
+  saveRDS(results$final.year.outputs$mass_results_offshore,str_glue("./fishing/fishing bounds/changingpower/biomassoutputs/inoff/{SimulationID}_offshore_{focal}.rds"))
+  saveRDS(results$final.year.outputs$mass_results_inshore,str_glue("./fishing/fishing bounds/changingpower/biomassoutputs/inoff/{SimulationID}_inshore_{focal}.rds"))
   saveRDS(results$final.year.outputs$mass_results_wholedomain,str_glue("./fishing/fishing bounds/changingpower/biomassoutputs/wholedomain/{SimulationID}_wholedomain_{focal}.rds"))
 }
 
 runs <- future_pmap(master,safely(focalgear),.progress = TRUE) #call the function in parallel
+saveRDS(runs,file = "runfails.Rds")
 toc() #time
